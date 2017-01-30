cf27d1540f63c6b1c66d5267ab9b3bb3aadf431d
rajughosh <raju.ghosh@fieldnation.com>
2017-01-30 20:30:12 +0600

    Change Jenkins file

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,14 +1,4 @@
 #!groovy
-node {
-	  stage('Build') { // <2>
-	   echo "Running ${currentBuild.displayName} on and ${env.JOB_NAME} and ${env.JOB_URL} on ${env.result}"
-   	 }
-  
-    stage('Test') {
-     	 echo "test success"
-    	}
+standardBuild{
 
-    stage('Deploy') {
-     	 echo "Deploy success"
-   	 }
 }
--- standardBuild.groovy
+++ standardBuild.groovy
@@ -1,19 +1,20 @@
-
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+
     stage 'checkout'
+
     node {
 	  checkout scm
 	  stage('Build') { // <2>
-	  echo "build success"
+	    echo "build success"
    	 }
-    	stage('Test') {
+    stage('Test') {
      	 echo "test success"
     	}
-    	stage('Deploy') {
+    stage('Deploy') {
      	 echo "Deploy success"
    	 }
    }
