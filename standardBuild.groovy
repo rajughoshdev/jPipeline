f9559c2387af41e416c3d8abf69bdc3f886b9c06
Raju Ghosh <rajughoshdev@gmail.com>
2017-01-30 19:15:10 +0600

    Update standardBuild.groovy

--- standardBuild.groovy
+++ standardBuild.groovy
@@ -1,16 +1,13 @@
 
-// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
-
-// The call(body) method in any file in workflowLibs.git/vars is exposed as a
-// method with the same name as the file.
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+    stage 'checkout'
     node {
 	    stage('Build') { // <2>
-				echo "build success"
+		echo "build success"
    	 }
     	stage('Test') {
      	 echo "test success"
