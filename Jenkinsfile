24cc3145c882d7ffea3edf2eb995114231445838
rajughosh <raju.ghosh@fieldnation.com>
2017-01-30 16:17:42 +0600

    added standardBuild

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,12 +1,3 @@
 #!groovy
-node { // <1>
-    stage('Build') { // <2>
-			echo "build success"
-    }
-    stage('Test') {
-      echo "test success"
-    }
-    stage('Deploy') {
-      echo "Deploy success"
-    }
+standardBuild { 
 }
---
+++ standardBuild.groovy
@@ -0,0 +1,23 @@
+
+// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
+
+// The call(body) method in any file in workflowLibs.git/vars is exposed as a
+// method with the same name as the file.
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    stage 'checkout'
+    node {
+	    stage('Build') { // <2>
+				echo "build success"
+   	 }
+    	stage('Test') {
+     	 echo "test success"
+    	}
+    	stage('Deploy') {
+     	 echo "Deploy success"
+   	 }
+   }
+}
