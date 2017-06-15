5ed5428b59b06b9865cda82c6345b47f8edd8e0c
Raju Ghosh <rajughoshdev@gmail.com>
2017-06-15 17:43:41 +0600

    Create standardBuild.groovy

--- standardBuild.groovy
+++ standardBuild.groovy
@@ -3,19 +3,7 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-
-    stage 'checkout'
-
     node {
-	  checkout scm
-	  stage('Build') { // <2>
-	    echo "build success"
-   	 }
-    stage('Test') {
-     	 echo "test success"
-    	}
-    stage('Deploy') {
-     	 echo "Deploy success"
-   	 }
+	  echo "build success"
    }
 }
