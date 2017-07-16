6b2ee3e938a23d66a138d248633da7e8616220ae
Raju Ghosh <rajughoshdev@gmail.com>
2017-07-16 19:04:04 +0600

    Update standardBuild.groovy

--- standardBuild.groovy
+++ standardBuild.groovy
@@ -3,8 +3,11 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+
+    // This is where the magic happens - put your pipeline snippets in here, get variables from config.
     node {
-	   checkout scm
-	  echo "build success"
-   }
+        git 'https://github.com/rajughoshdev/docker-compose-workshop.git'
+        sh 'cat Dockerfile'
+   
+    }
 }
